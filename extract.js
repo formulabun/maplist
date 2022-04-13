1a4384aa1d8ad4784a64c56563b1ec52cd9c8211
Fl_GUI <flor.guilini@hotmail.com>
2022-04-13 21:13:51 +0200

    extrat level headers

---
+++ extract.js
@@ -0,0 +1,25 @@
+import {openFile} from "srb2kartjs";
+import {writeFile} from "fs/promises";
+import {basename, extname} from "path";
+
+
+await Promise.all(
+  process.argv.slice(2).map(filename => {
+    openFile(filename)
+      .then(file => file.getAllSocs())
+      .then(soc => soc.level)
+      .then(levels => {
+        const diffs = [];
+        for (let id in levels) {
+          diffs.push(`LEVEL MAP${id.toUpperCase()}`);
+        }
+        return diffs;
+      })
+      .then(levelheaders => {
+        const base = basename(filename, extname(filename));
+        let packname = base.split("_")[1];
+        if(extname(filename) === ".kart") packname = base;
+        return writeFile(`socdiffs/${packname}.socdiff`, levelheaders.join("\n"));
+      })
+  })
+);
